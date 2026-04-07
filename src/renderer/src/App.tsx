e1a955b0b7dbb739299a0e14f138d87e2a3347a1
Taariq Elliott <codebytaariq@gmail.com>
2026-04-07 08:06:26 -0400

    Added shadcn

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,16 @@
+import { ArrowUpIcon } from 'lucide-react'
+import { Button } from './components/ui/button'
+
 export default function App() {
   return (
     <div className="bg-[#1414cf] h-screen w-full justify-center items-center flex flex-col">
       <p className="text-lime-400">Audio Player v2.1</p>
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <Button variant="outline">Button</Button>
+        <Button variant="outline" size="icon" aria-label="Submit">
+          <ArrowUpIcon />
+        </Button>
+      </div>
     </div>
   )
 }
