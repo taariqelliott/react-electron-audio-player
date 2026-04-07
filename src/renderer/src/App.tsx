f4c7439a7d19bd07c33360465540c93704e43f8a
Taariq Elliott <codebytaariq@gmail.com>
2026-04-07 07:44:30 -0400

    Initial commit for audio player + tailwind

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-[#1414cf] h-screen w-full justify-center items-center flex flex-col">
+      <p className="text-lime-400">Audio Player v2.1</p>
+    </div>
+  )
+}
